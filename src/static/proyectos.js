b13da714da12509f505d930363cb1ac488a9b3f5
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-15 02:21:06 -0300

    Se actualizan las experiencias que se presentan

--- src/static/proyectos.js
+++ src/static/proyectos.js
@@ -1,19 +1,57 @@
 import portafolio from '../static/portafolio.png'
+import authapp from '../static/auth-app.png'
+import ceewebapp from '../static/ceewebapp.png'
 
 export const proyectos = [
     {
         "imagen": portafolio,
-        "descripcion": "Practica Profesional I desarrollando páginas web",
-        "titulo": "portafolio",
+        "descripcion": "Portafolio personal desarrollado con React y ChakraUI. Se desarrollo con el fin de presentar todas las tecnologías que he aprendido y que he utilizado en mis proyectos",
+        "titulo": "Portafolio",
         "tecnologias": [
             "React",
             "ChakraUI",
             "JavaScript",
             "Git",
             "GitHub",
-            "Visual Studio Code"
+            "Visual Studio Code",
         ],
         "repositorio": "https://github.com/Xhyus/portfolio",
         "url": "https://www.ignaciogonzalez.dev"
-    }
+    },
+    {
+        "imagen": authapp,
+        "descripcion": "Proyecto de autenticación con uso de cookies, JWT y uso de rutas protegidas, se utilizo para enseñarles a los alumnos de las ayudantías",
+        "titulo": "Auth-App",
+        "tecnologias": [
+            "Next.JS",
+            "ChakraUI",
+            "JavaScript",
+            "Express",
+            "Mongoose",
+            "MongoDB",
+            "Git",
+            "GitHub",
+            "Visual Studio Code",
+        ],
+        "repositorio": "https://github.com/Xhyus/Auth_cookies",
+        "url": "unset"
+    },
+    {
+        "imagen": ceewebapp,
+        "descripcion": "Proyecto desarrollado para la asignatura de Taller de desarrollo en conjunto con mi equipo de trabajo. Se desarrollo una aplicación web para los Centros de estudiantes de la Universidad del Bío-Bío",
+        "titulo": "CeeWebApp",
+        "tecnologias": [
+            "Next.JS",
+            "ChakraUI",
+            "JavaScript",
+            "Express",
+            "Mongoose",
+            "MongoDB",
+            "Git",
+            "GitHub",
+            "Visual Studio Code",
+        ],
+        "repositorio": "https://github.com/Xhyus/ceewebapp",
+        "url": "unset"
+    },
 ]
