286285253d51e2e5b6ac6605afd2049a532eab7b
Ignacio Gonzalez <ignaciogonzalez1609@gmail.com>
2023-02-04 01:12:34 -0300

    Modificacion: Se modifica color de fondo de página y se inhabilitan temporalmente los componentes no modificados

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import Inicio from './views/Inicio';
-import { ChakraProvider, Container } from '@chakra-ui/react'
-import Navbar from './components/Navbar';
-import theme from './theme'
-import { ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<ChakraProvider>
-			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-			<Navbar />
-			<Container maxW={"full"} backgroundColor={"#264E70"} maxH={"full"}>
-				<Inicio />
-			</Container >
+			<Inicio />
 		</ChakraProvider>
-	</React.StrictMode >
+	</StrictMode >
 );
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
 import { Container, } from '@chakra-ui/react'
-import Empresas from '../components/Empresas'
-import Tecnologias from '../components/Tecnologias'
-import Educacion from '../components/Educacion'
+import Navbar from '../components/Navbar'
+// import Empresas from '../components/Empresas'
+// import Tecnologias from '../components/Tecnologias'
+// import Educacion from '../components/Educacion'
 import SobreMi from '../components/SobreMi'
-import Proyectos from '../components/Proyectos'
-import Idiomas from '../components/Idiomas'
+// import Proyectos from '../components/Proyectos'
+// import Idiomas from '../components/Idiomas'
 
 const Inicio = () => {
     return (
-        <Container maxW={"container.lg"} py={20}>
-            <SobreMi />
-            <Educacion />
+        <>
+            <Navbar />
+            <Container maxW={"container.xl"} py={20}>
+                <SobreMi />
+                {/*<Educacion />
             <Empresas />
             <Idiomas />
             <Tecnologias />
-            <Proyectos />
-        </Container >
+        <Proyectos /> */}
+            </Container >
+        </>
     )
 }
 
