b488b3bb3a8f376b9e3dc1034668a977358f882b
Ignacio Gonzalez <ignaciogonzalez1609@gmail.com>
2023-02-04 01:10:11 -0300

    Modificación: Se agregan imagenes de experiencias y se modifican los archivos json para funcionar con las imagenes incrustadas

--- src/static/experiencias.js
+++ src/static/experiencias.js
@@ -1,24 +1,29 @@
-[
+import fragua from './fragua.png'
+import pryx from './pryx.png'
+import ubb from './ubb.png'
+import mibo from './mibo.png'
+
+const experiencias = [
     {
         "experienciasDestacables": [
             {
                 "empresa": "Estudio Fragua",
-                "descripcion": "Proyecto de título enfocado en el desarrollo de una plataforma de cotizaciones. Este proyecto se esta desarrollando con el stack MERN y Chakra UI.",
-                "logo": "http://ignaciogonzalez.dev/fragua.png",
+                "descripcion": "Proyecto de título enfocado en el desarrollo de una plataforma de cotizaciones. Este proyecto se desarrolló con el stack MERN usando Next.JS y Chakra UI.",
+                "logo": fragua,
                 "size": "70%",
                 "periodo": "Septiembre 2022 - Enero 2023"
             },
             {
                 "empresa": "Pryx",
                 "descripcion": "Practica Profesional II enfocada en el desarrollando páginas web con Wordpress, CSS y PHP. Además se desarrollaron aplicaciones móviles con React Native y CSS",
-                "logo": "https://fpymebiobio.cl/wp-content/uploads/2022/05/Pryx-logo-b.png",
+                "logo": pryx,
                 "size": "50%",
                 "periodo": "Enero 2022 - Marzo 2022"
             },
             {
                 "empresa": "Pryx",
                 "descripcion": "Practica Profesional I enfocada en el desarrollo de páginas web con Wordpress y CSS. Además de un sistema de gestion de inventario desarrollado con Django.",
-                "logo": "https://fpymebiobio.cl/wp-content/uploads/2022/05/Pryx-logo-b.png",
+                "logo": pryx,
                 "size": "50%",
                 "periodo": "Junio 2021 - Septiembre 2021"
             }
@@ -28,25 +33,27 @@
         "trabajosFormales": [
             {
                 "empresa": "UBB",
-                "descripcion": "Ayudantías para Ingeniería de Ejecución e Informática y para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocan en el aprendizaje del stack MERN",
-                "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Escudo_Universidad_del_B%C3%ADo-B%C3%ADo.png",
+                "descripcion": "Ayudantías para Ingeniería de Ejecución e Informática y para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocan en el aprendizaje del stack MERN.",
+                "logo": ubb,
                 "size": "40%",
                 "periodo": "Septiembre 2022 - Enero 2023"
             },
             {
                 "empresa": "UBB",
-                "descripcion": "Ayudantías para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocaron en aprender PHP",
-                "logo": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Escudo_Universidad_del_B%C3%ADo-B%C3%ADo.png",
+                "descripcion": "Ayudantías para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocaron en el uso del patron de desarrollo MVC mediante PHP, HTML y CSS.",
+                "logo": ubb,
                 "size": "40%",
                 "periodo": "Abril 2022 - Agosto 2022"
             },
             {
                 "empresa": "MIBO",
-                "descripcion": "Apoyo en certificaciones para empresas y que estas puedan emitir boletas mediante SII",
-                "logo": "https://mibo.cl/homepage/images/mibo.png",
+                "descripcion": "Apoyo en certificaciones de empresas para el uso de boletas electronicas con SII mediante MIBO.",
+                "logo": mibo,
                 "size": "30%",
                 "periodo": "Mayo 2020 - Febrero 2021"
             }
         ]
     }
 ]
+
+export default experiencias
---
+++ src/static/textos-estaticos.js
@@ -0,0 +1,5 @@
+export const sobre_mi = [
+    "Egresado de Ingeniería en Ejecución Informática de la Universidad del Bío-Bío en proceso de obtención de título, con ganas de siempre aprender algo nuevo o mejorando las cosas que sé, entusiasta, autodidacta y analítico. Con experiencia en desarrollo web y software a la medida.",
+    "Me gusta mucho el desarrollo web, puedo programar frontend y backend de los proyectos, suelo refactorizar mis codigos para optimizarlo en la medida de lo que voy aprendiendo.",
+    "El último proyecto que desarrollé es una plataforma web para realizar cotizaciones de forma sencilla para una empresa, utilizando NextJS, NodeJS, Express, Mongoose(MongoDB) y ChakraUI."
+]
