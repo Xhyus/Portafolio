bad4c4c7f35429e4d853894e3b1bb185d103b67b
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-02-21 22:18:34 -0300

    Actualizacion: Se modifican los archivos json con la informacion actualizada

--- src/static/experiencias.js
+++ src/static/experiencias.js
@@ -3,57 +3,54 @@ import pryx from './pryx.png'
 import ubb from './ubb.png'
 import mibo from './mibo.png'
 
-const experiencias = [
-    {
-        "experienciasDestacables": [
-            {
-                "empresa": "Estudio Fragua",
-                "descripcion": "Proyecto de título enfocado en el desarrollo de una plataforma de cotizaciones. Este proyecto se desarrolló con el stack MERN usando Next.JS y Chakra UI.",
-                "logo": fragua,
-                "size": "70%",
-                "periodo": "Septiembre 2022 - Enero 2023"
-            },
-            {
-                "empresa": "Pryx",
-                "descripcion": "Practica Profesional II enfocada en el desarrollando páginas web con Wordpress, CSS y PHP. Además se desarrollaron aplicaciones móviles con React Native y CSS",
-                "logo": pryx,
-                "size": "50%",
-                "periodo": "Enero 2022 - Marzo 2022"
-            },
-            {
-                "empresa": "Pryx",
-                "descripcion": "Practica Profesional I enfocada en el desarrollo de páginas web con Wordpress y CSS. Además de un sistema de gestion de inventario desarrollado con Django.",
-                "logo": pryx,
-                "size": "50%",
-                "periodo": "Junio 2021 - Septiembre 2021"
-            }
-        ]
-    },
-    {
-        "trabajosFormales": [
-            {
-                "empresa": "UBB",
-                "descripcion": "Ayudantías para Ingeniería de Ejecución e Informática y para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocan en el aprendizaje del stack MERN.",
-                "logo": ubb,
-                "size": "40%",
-                "periodo": "Septiembre 2022 - Enero 2023"
-            },
-            {
-                "empresa": "UBB",
-                "descripcion": "Ayudantías para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocaron en el uso del patron de desarrollo MVC mediante PHP, HTML y CSS.",
-                "logo": ubb,
-                "size": "40%",
-                "periodo": "Abril 2022 - Agosto 2022"
-            },
-            {
-                "empresa": "MIBO",
-                "descripcion": "Apoyo en certificaciones de empresas para el uso de boletas electronicas con SII mediante MIBO.",
-                "logo": mibo,
-                "size": "30%",
-                "periodo": "Mayo 2020 - Febrero 2021"
-            }
-        ]
-    }
-]
+const experiencias = {
+    "experienciasDestacables": [
+        {
+            "empresa": "Estudio Fragua",
+            "descripcion": "Proyecto de título enfocado en el desarrollo de una plataforma de cotizaciones. Este proyecto se desarrolló con el stack MERN usando Next.JS y Chakra UI.",
+            "logo": fragua,
+            "size": "70%",
+            "periodo": "Septiembre 2022 - Enero 2023"
+        },
+        {
+            "empresa": "Pryx",
+            "descripcion": "Practica Profesional II enfocada en el desarrollando páginas web con Wordpress, CSS y PHP. Además se desarrollaron aplicaciones móviles con React Native y CSS",
+            "logo": pryx,
+            "size": "50%",
+            "periodo": "Enero 2022 - Marzo 2022"
+        },
+        {
+            "empresa": "Pryx",
+            "descripcion": "Practica Profesional I enfocada en el desarrollo de páginas web con Wordpress y CSS. Además de un sistema de gestion de inventario desarrollado con Django.",
+            "logo": pryx,
+            "size": "50%",
+            "periodo": "Junio 2021 - Septiembre 2021"
+        }
+    ],
+
+    "trabajosFormales": [
+        {
+            "empresa": "UBB",
+            "descripcion": "Ayudantías para Ingeniería de Ejecución e Informática y para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocan en el aprendizaje del stack MERN.",
+            "logo": ubb,
+            "size": "40%",
+            "periodo": "Septiembre 2022 - Enero 2023"
+        },
+        {
+            "empresa": "UBB",
+            "descripcion": "Ayudantías para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocaron en el uso del patron de desarrollo MVC mediante PHP, HTML y CSS.",
+            "logo": ubb,
+            "size": "40%",
+            "periodo": "Abril 2022 - Agosto 2022"
+        },
+        {
+            "empresa": "MIBO",
+            "descripcion": "Apoyo en certificaciones de empresas para el uso de boletas electronicas con SII mediante MIBO.",
+            "logo": mibo,
+            "size": "30%",
+            "periodo": "Mayo 2020 - Febrero 2021"
+        }
+    ],
+}
 
 export default experiencias
--- src/static/tecnologias.js
+++ src/static/tecnologias.js
@@ -3,81 +3,68 @@ import { SiNextdotjs, SiExpress, SiMongodb, SiMysql, SiChakraui, SiJavascript, S
 import { TbBrandReactNative } from 'react-icons/tb'
 
 export const frontend = [{
-    icono: FaReact,
     tecnologia: "ReactJS",
     color: "blue.500",
     documentacion: "https://es.reactjs.org/docs/getting-started.html"
 },
 {
-    icono: SiNextdotjs,
     tecnologia: "Next.Js",
     color: "white",
     documentacion: "https://nextjs.org/docs"
 },
 {
-    icono: TbBrandReactNative,
     tecnologia: "React Native",
     color: "purple.500",
     documentacion: "https://reactnative.dev/docs/next/components-and-apis"
 },
 {
-    icono: SiChakraui,
     tecnologia: "Chakra UI",
     color: "teal.500",
     documentacion: "https://chakra-ui.com/docs/getting-started"
 },
 {
-    icono: FaHtml5,
     tecnologia: "HTML",
     color: "orange.500",
     documentacion: "https://developer.mozilla.org/es/docs/Web/HTML"
 },
 {
-    icono: FaCss3,
     tecnologia: "CSS",
     color: "blue.500",
     documentacion: "https://developer.mozilla.org/es/docs/Web/CSS"
 },
 {
-    icono: FaBootstrap,
     tecnologia: "Bootstrap",
     color: "purple.400",
     documentacion: "https://bootstrap.com/v5/docs/"
 }]
 
 export const backend = [{
-    icono: SiExpress,
     tecnologia: "ExpressJS",
     color: "",
     documentacion: ""
 },
 {
-    icono: FaNodeJs,
     tecnologia: "NodeJS",
     color: "",
     documentacion: ""
 }]
 
 export const herramientas = [{
-    icono: FaGithub,
     tecnologia: "Github",
     color: "",
     documentacion: ""
 },
 {
-    icono: FaGitAlt,
     tecnologia: "Git",
     color: "",
     documentacion: ""
 },
 {
-    icono: SiInsomnia,
     tecnologia: "Insomnia REST",
     color: "",
     documentacion: ""
 },
 {
-    icono: SiVisualstudiocode,
     tecnologia: "Visual Studio Code",
     color: "",
     documentacion: ""
@@ -86,30 +73,27 @@ export const herramientas = [{
 export const lenguajes = [{
     icono: FaPhp,
     tecnologia: "PHP",
-    color: "",
-    documentacion: ""
+    color: "purple.500",
+    documentacion: "https://www.php.net/docs.php"
 },
 {
     icono: SiJavascript,
     tecnologia: "Javascript",
-    color: "",
-    documentacion: ""
+    color: "yellow.400",
+    documentacion: "https://devdocs.io/javascript/"
 }]
 
 export const base_datos = [{
-    icono: SiPostgresql,
     tecnologia: "PostgresSQL",
     color: "",
     documentacion: ""
 },
 {
-    icono: SiMysql,
     tecnologia: "MySQL",
     color: "",
     documentacion: ""
 },
 {
-    icono: SiMongodb,
     tecnologia: "MongoDB",
     color: "",
     documentacion: ""
