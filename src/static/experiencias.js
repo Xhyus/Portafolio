d2de8582a7e26f9912e79aabc3a5b75ee077908d
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-02-23 22:47:50 -0300

    Actualizacion: Se agregan referencias a las experiencias de trabajo

--- src/static/experiencias.js
+++ src/static/experiencias.js
@@ -10,21 +10,33 @@ const experiencias = {
             "descripcion": "Proyecto de título enfocado en el desarrollo de una plataforma de cotizaciones. Este proyecto se desarrolló con el stack MERN usando Next.JS y Chakra UI.",
             "logo": fragua,
             "size": "70%",
-            "periodo": "Septiembre 2022 - Enero 2023"
+            "periodo": "Septiembre 2022 - Enero 2023",
+            "referencia": true,
+            "nombre": "Cristopher Novoa",
+            "telefono": "+56983948854",
+            "correo": "cnovoa@pryx.cl"
         },
         {
             "empresa": "Pryx",
             "descripcion": "Practica Profesional II enfocada en el desarrollando páginas web con Wordpress, CSS y PHP. Además se desarrollaron aplicaciones móviles con React Native y CSS",
             "logo": pryx,
             "size": "50%",
-            "periodo": "Enero 2022 - Marzo 2022"
+            "periodo": "Enero 2022 - Marzo 2022",
+            "referencia": true,
+            "nombre": "Cristopher Novoa",
+            "telefono": "+56983948854",
+            "correo": "cnovoa@pryx.cl"
         },
         {
             "empresa": "Pryx",
             "descripcion": "Practica Profesional I enfocada en el desarrollo de páginas web con Wordpress y CSS. Además de un sistema de gestion de inventario desarrollado con Django.",
             "logo": pryx,
             "size": "50%",
-            "periodo": "Junio 2021 - Septiembre 2021"
+            "periodo": "Junio 2021 - Septiembre 2021",
+            "referencia": true,
+            "nombre": "Cristopher Novoa",
+            "telefono": "+56983948854",
+            "correo": "cnovoa@pryx.cl"
         }
     ],
 
@@ -34,21 +46,33 @@ const experiencias = {
             "descripcion": "Ayudantías para Ingeniería de Ejecución e Informática y para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocan en el aprendizaje del stack MERN.",
             "logo": ubb,
             "size": "40%",
-            "periodo": "Septiembre 2022 - Enero 2023"
+            "periodo": "Septiembre 2022 - Enero 2023",
+            "referencia": true,
+            "nombre": "Cristopher Novoa",
+            "telefono": "+56948903816",
+            "correo": "asegura@ubiobio.cl"
         },
         {
             "empresa": "UBB",
             "descripcion": "Ayudantías para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocaron en el uso del patron de desarrollo MVC mediante PHP, HTML y CSS.",
             "logo": ubb,
             "size": "40%",
-            "periodo": "Abril 2022 - Agosto 2022"
+            "periodo": "Abril 2022 - Agosto 2022",
+            "referencia": true,
+            "nombre": "Alejandra Segura",
+            "telefono": "+56948903816",
+            "correo": "asegura@ubiobio.cl"
         },
         {
             "empresa": "MIBO",
             "descripcion": "Apoyo en certificaciones de empresas para el uso de boletas electronicas con SII mediante MIBO.",
             "logo": mibo,
             "size": "30%",
-            "periodo": "Mayo 2020 - Febrero 2021"
+            "periodo": "Mayo 2020 - Febrero 2021",
+            "referencia": false,
+            "nombre": "",
+            "telefono": "",
+            "correo": ""
         }
     ],
 }
