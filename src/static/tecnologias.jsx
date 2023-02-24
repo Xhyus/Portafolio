f8aa3f6245a05f871acb0f45d0d59c08bbd581e9
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-02-23 22:46:15 -0300

    Se agregan las tecnologias dominadas junto a sus iconos

---
+++ src/static/tecnologias.jsx
@@ -0,0 +1,116 @@
+import { FaReact, FaNodeJs, FaGitAlt, FaPhp, FaCss3, FaHtml5, FaGithub, FaBootstrap } from 'react-icons/fa'
+import { SiNextdotjs, SiExpress, SiMongodb, SiMysql, SiChakraui, SiJavascript, SiPostgresql, SiInsomnia, SiVisualstudiocode } from 'react-icons/si'
+import { TbBrandReactNative } from 'react-icons/tb'
+
+export const frontend = [{
+    icono: <FaReact />,
+    tecnologia: "ReactJS",
+    color: "blue.500",
+    documentacion: "https://es.reactjs.org/docs/getting-started.html"
+},
+{
+    icono: <SiNextdotjs />,
+    tecnologia: "Next.Js",
+    color: "white",
+    documentacion: "https://nextjs.org/docs"
+},
+{
+    icono: <TbBrandReactNative />,
+    tecnologia: "React Native",
+    color: "purple.500",
+    documentacion: "https://reactnative.dev/docs/next/components-and-apis"
+},
+{
+    icono: <SiChakraui />,
+    tecnologia: "Chakra UI",
+    color: "teal.500",
+    documentacion: "https://chakra-ui.com/docs/getting-started"
+},
+{
+    icono: <FaHtml5 />,
+    tecnologia: "HTML",
+    color: "orange.500",
+    documentacion: "https://developer.mozilla.org/es/docs/Web/HTML"
+},
+{
+    icono: <FaCss3 />,
+    tecnologia: "CSS",
+    color: "blue.500",
+    documentacion: "https://developer.mozilla.org/es/docs/Web/CSS"
+},
+{
+    icono: <FaBootstrap />,
+    tecnologia: "Bootstrap",
+    color: "purple.400",
+    documentacion: "https://bootstrap.com/v4/docs/"
+}]
+
+export const backend = [{
+    icono: <SiExpress />,
+    tecnologia: "ExpressJS",
+    color: "",
+    documentacion: ""
+},
+{
+    icono: <FaNodeJs />,
+    tecnologia: "NodeJS",
+    color: "",
+    documentacion: ""
+}]
+
+export const herramientas = [{
+    icono: <FaGithub />,
+    tecnologia: "Github",
+    color: "",
+    documentacion: ""
+},
+{
+    icono: <FaGitAlt />,
+    tecnologia: "Git",
+    color: "",
+    documentacion: ""
+},
+{
+    icono: <SiInsomnia />,
+    tecnologia: "Insomnia REST",
+    color: "",
+    documentacion: ""
+},
+{
+    icono: <SiVisualstudiocode />,
+    tecnologia: "Visual Studio Code",
+    color: "",
+    documentacion: ""
+}]
+
+export const lenguajes = [{
+    icono: <FaPhp />,
+    tecnologia: "PHP",
+    color: "purple.500",
+    documentacion: "https://www.php.net/docs.php"
+},
+{
+    icono: <SiJavascript />,
+    tecnologia: "Javascript",
+    color: "yellow.400",
+    documentacion: "https://devdocs.io/javascript/"
+}]
+
+export const base_datos = [{
+    icono: <SiPostgresql />,
+    tecnologia: "PostgresSQL",
+    color: "",
+    documentacion: ""
+},
+{
+    icono: <SiMysql />,
+    tecnologia: "MySQL",
+    color: "",
+    documentacion: ""
+},
+{
+    icono: <SiMongodb />,
+    tecnologia: "MongoDB",
+    color: "",
+    documentacion: ""
+}]
