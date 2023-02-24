c8536f106a19d1e7b59cb1e53a88c547f35be1ac
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-02-24 16:26:15 -0300

    Modificacion: Se modifica el manejo de la tabla de conocimientos y se genera componente generico tipo tabla

--- src/components/Conocimientos.jsx
+++ src/components/Conocimientos.jsx
@@ -1,112 +1,33 @@
-import { Stack, Text, Wrap, WrapItem, Link, GridItem } from '@chakra-ui/react'
-import { FaReact, FaNodeJs, FaGitAlt, FaPhp, FaCss3, FaHtml5, FaGithub } from 'react-icons/fa'
-import { SiNextdotjs, SiExpress, SiMongodb, SiMysql, SiChakraui, SiJavascript, SiPostgresql, SiInsomnia, SiVisualstudiocode } from 'react-icons/si'
-import { TbBrandReactNative } from 'react-icons/tb'
+import { Stack, GridItem, Grid } from '@chakra-ui/react'
 import SubTitulo from './generic_components/SubTitulo'
-import TagIcon from './generic_components/TagIcon'
-
-const Iconos = ({ icon, url, tecnologia, color }) => {
-    return (
-        <GridItem w={"fit-content"} >
-            <Link href={url} isExternal>
-                <TagIcon Icono={icon} tag={tecnologia} color={color} />
-            </Link>
-        </GridItem>
-    )
-}
+import TablaConocimientos from './generic_components/TablaConocimientos'
+import { frontend, backend, base_datos, herramientas, lenguajes } from '../static/tecnologias'
 
 const Conocimientos = () => {
     return (
         <Stack w={"full"} py={5} id="Conocimientos">
             <SubTitulo type="h2" size="2xl" data="CONOCIMIENTOS" />
-            <Wrap h="full" align="flex-start" w="full" justify="space-between">
-                <WrapItem>
-                    <Stack border="1px solid black" w="full">
-                        <Text fontSize="xl" borderBottom="1px solid black" p={2} textAlign="center" fontWeight="semibold" >Lenguajes de<br /> Programación</Text>
-                        <Wrap justify="center" spacing={5} py="2" px="3">
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiJavascript} url="https://developer.mozilla.org/es/docs/Web/JavaScript" tecnologia="JavaScript" color="yellow.400" />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaPhp} url="https://www.php.net/" tecnologia="PHP" color="blue.400" />
-                            </WrapItem>
-                        </Wrap>
-                    </Stack>
-                </WrapItem>
-                <WrapItem>
-                    <Stack border="1px solid black" w="full">
-                        <Text fontSize="xl" borderBottom="1px solid black" p={2} textAlign="center" fontWeight="semibold" >Frontend</Text>
-                        <Wrap justify="center" spacing={5} py="2" px="3" maxW="xl">
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaReact} tecnologia="React" url="https://es.reactjs.org/docs/getting-started.html" color={"blue.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiNextdotjs} tecnologia="NextJS" url="https://nextjs.org/docs" color={"white"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={TbBrandReactNative} tecnologia="React Native" url="https://reactnative.dev/docs/next/components-and-apis" color={"purple.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiChakraui} tecnologia="ChakraUI" url="https://chakra-ui.com/docs/getting-started" color={"teal.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaHtml5} tecnologia="HTML" url="https://developer.mozilla.org/es/docs/Web/HTML" color={"orange.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaCss3} tecnologia="CSS" url="https://developer.mozilla.org/es/docs/Web/CSS" color={"blue.500"} />
-                            </WrapItem>
-                        </Wrap>
-                    </Stack>
-                </WrapItem>
-                <WrapItem>
-                    <Stack border="1px solid black" w="full">
-                        <Text fontSize="xl" borderBottom="1px solid black" p={2} textAlign="center" fontWeight="semibold" >Backend</Text>
-                        <Wrap justify="center" spacing={5} py="2" px="3">
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaNodeJs} tecnologia="NodeJS" url="https://nodejs.org/dist/latest-v16.x/docs/api/" color={"green.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiExpress} tecnologia="Express" url="https://expressjs.com/es/api.html" color={"red.500"} />
-                            </WrapItem>
-                        </Wrap>
-                    </Stack>
-                </WrapItem>
-                <WrapItem>
-                    <Stack border="1px solid black" w="full">
-                        <Text fontSize="xl" borderBottom="1px solid black" p={2} textAlign="center" fontWeight="semibold" >Bases de datos</Text>
-                        <Wrap justify="center" spacing={5} py="2" px="3">
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiMongodb} tecnologia="MongoDB" url="https://docs.mongodb.com/" color={"green.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiMysql} tecnologia="MySQL" url="https://dev.mysql.com/doc/" color={"blue.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiPostgresql} tecnologia="PostgreSQL" url="https://www.postgresql.org/docs/" color={"blue.500"} />
-                            </WrapItem>
-                        </Wrap>
-                    </Stack>
-                </WrapItem>
-                <WrapItem>
-                    <Stack border="1px solid black" w="full">
-                        <Text fontSize="xl" borderBottom="1px solid black" p={2} textAlign="center" fontWeight="semibold" >Herramientas</Text>
-                        <Wrap justify="center" spacing={5} py="2" px="3">
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaGitAlt} tecnologia="Git" url="https://git-scm.com/docs" color={"red.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={FaGithub} tecnologia="GitHub" url="https://docs.github.com/es" color={"white"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiInsomnia} tecnologia="Insomnia" url="https://support.insomnia.rest/" color={"purple.500"} />
-                            </WrapItem>
-                            <WrapItem w={"fit-content"} >
-                                <Iconos icon={SiVisualstudiocode} tecnologia="Visual Studio Code" url="https://code.visualstudio.com/docs" color={"blue.500"} />
-                            </WrapItem>
-                        </Wrap>
-                    </Stack>
-                </WrapItem>
-            </Wrap>
+            <Grid gap={5}>
+                <Grid templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(7, 1fr)" }} gap={5} w={"full"} >
+                    <GridItem colSpan={{ base: 2, md: 2 }} >
+                        <TablaConocimientos header="Lenguajes de Programación" tecnologias={lenguajes} />
+                    </GridItem>
+                    <GridItem colSpan={{ base: 2, md: 3 }} >
+                        <TablaConocimientos header="Frontend" tecnologias={frontend} />
+                    </GridItem>
+                    <GridItem colSpan={{ base: 2, md: 2 }} >
+                        <TablaConocimientos header="Backend" tecnologias={backend} />
+                    </GridItem>
+                </Grid>
+                <Grid templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }} gap={5} w={"full"} >
+                    <GridItem colSpan={{ base: 2, md: 1 }} >
+                        <TablaConocimientos header="Base de Datos" tecnologias={base_datos} />
+                    </GridItem>
+                    <GridItem colSpan={{ base: 2, md: 2 }} >
+                        <TablaConocimientos header="Herramientas" tecnologias={herramientas} />
+                    </GridItem>
+                </Grid>
+            </Grid>
         </Stack >
     )
 }
--- src/components/generic_components/TablaConocimientos.jsx
+++ src/components/generic_components/TablaConocimientos.jsx
@@ -1,29 +1,23 @@
-import { WrapItem, Stack, Grid, Text } from '@chakra-ui/react'
+import { Stack, Text, Link, GridItem, Wrap } from '@chakra-ui/react'
 import TagIcon from './TagIcon'
 
-const Iconos = ({ icon, url, tecnologia, color }) => {
-    return (
-        <GridItem w={"fit-content"} >
-            <Link href={url} isExternal>
-                <TagIcon Icono={icon} tag={tecnologia} color={color} />
-            </Link>
-        </GridItem>
-    )
-}
 
 const TablaConocimientos = ({ header, tecnologias }) => {
     return (
-        <WrapItem>
-            <Stack border="2px gray solid" borderRadius="3px" w="content-fit">
-                <Text p={2} textAlign="center" fontWeight="semibold">Lenguajes de<br /> programación</Text>
-                <Stack borderTop="2px gray solid" p={5}>
-                    <Grid gap={1} templateColumns="repeat(2, 2fr)">
-                        <Iconos icon={SiJavascript} tecnologia="JavaScript" url="https://devdocs.io/javascript/" color={"yellow.400"} />
-                        <Iconos icon={FaPhp} tecnologia="PHP" url="https://www.php.net/docs.php" color={"purple.500"} />
-                    </Grid>
-                </Stack>
-            </Stack>
-        </WrapItem>
+        <Stack border="1px solid black" w="fit-content" borderRadius={12}>
+            <Text fontSize="xl" borderBottom="1px solid black" p={2} textAlign="center" fontWeight="semibold" >{header}</Text>
+            <Wrap justify="center" spacing={5} py="2" px="3">
+                {tecnologias.map((tecnologia) => {
+                    return (
+                        <GridItem w={"fit-content"} >
+                            <Link href={tecnologia.documentacion} isExternal>
+                                <TagIcon icono={tecnologia.icono} tag={tecnologia.tecnologia} color={tecnologia.color} />
+                            </Link>
+                        </GridItem>
+                    )
+                })}
+            </Wrap>
+        </Stack>
     )
 }
 
