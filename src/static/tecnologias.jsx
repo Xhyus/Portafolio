ed51e3964d3ebf257e1a3fbd2b567305ea4a719e
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-02-24 15:16:44 -0300

    Actualizacion: Se actualizan los datos estaticos de experiencias y tecnologias

--- src/static/tecnologias.jsx
+++ src/static/tecnologias.jsx
@@ -1,116 +1,130 @@
-import { FaReact, FaNodeJs, FaGitAlt, FaPhp, FaCss3, FaHtml5, FaGithub, FaBootstrap } from 'react-icons/fa'
-import { SiNextdotjs, SiExpress, SiMongodb, SiMysql, SiChakraui, SiJavascript, SiPostgresql, SiInsomnia, SiVisualstudiocode } from 'react-icons/si'
+import { FaReact, FaNodeJs, FaGitAlt, FaPhp, FaCss3, FaHtml5, FaGithub, FaBootstrap, FaJava } from 'react-icons/fa'
+import { SiNextdotjs, SiExpress, SiMongodb, SiMysql, SiChakraui, SiJavascript, SiPostgresql, SiInsomnia, SiVisualstudiocode, SiDjango } from 'react-icons/si'
 import { TbBrandReactNative } from 'react-icons/tb'
 
 export const frontend = [{
-    icono: <FaReact />,
+    icono: FaReact,
     tecnologia: "ReactJS",
     color: "blue.500",
     documentacion: "https://es.reactjs.org/docs/getting-started.html"
 },
 {
-    icono: <SiNextdotjs />,
+    icono: SiNextdotjs,
     tecnologia: "Next.Js",
     color: "white",
     documentacion: "https://nextjs.org/docs"
 },
 {
-    icono: <TbBrandReactNative />,
+    icono: TbBrandReactNative,
     tecnologia: "React Native",
     color: "purple.500",
     documentacion: "https://reactnative.dev/docs/next/components-and-apis"
 },
 {
-    icono: <SiChakraui />,
+    icono: SiChakraui,
     tecnologia: "Chakra UI",
     color: "teal.500",
     documentacion: "https://chakra-ui.com/docs/getting-started"
 },
 {
-    icono: <FaHtml5 />,
+    icono: FaHtml5,
     tecnologia: "HTML",
     color: "orange.500",
     documentacion: "https://developer.mozilla.org/es/docs/Web/HTML"
 },
 {
-    icono: <FaCss3 />,
+    icono: FaCss3,
     tecnologia: "CSS",
     color: "blue.500",
     documentacion: "https://developer.mozilla.org/es/docs/Web/CSS"
 },
 {
-    icono: <FaBootstrap />,
+    icono: FaBootstrap,
     tecnologia: "Bootstrap",
     color: "purple.400",
     documentacion: "https://bootstrap.com/v4/docs/"
 }]
 
 export const backend = [{
-    icono: <SiExpress />,
+    icono: SiExpress,
     tecnologia: "ExpressJS",
-    color: "",
-    documentacion: ""
+    color: "red.500",
+    documentacion: "https://expressjs.com/es/api.html"
 },
 {
-    icono: <FaNodeJs />,
+    icono: FaNodeJs,
     tecnologia: "NodeJS",
-    color: "",
-    documentacion: ""
-}]
+    color: "green.500",
+    documentacion: "https://nodejs.org/dist/latest-v16.x/docs/api/"
+},
+{
+    icono: SiDjango,
+    tecnologia: "Django",
+    color: "green.500",
+    documentacion: "https://docs.djangoproject.com/en/3.2/"
+}
+]
 
 export const herramientas = [{
-    icono: <FaGithub />,
+    icono: FaGithub,
     tecnologia: "Github",
-    color: "",
-    documentacion: ""
+    color: "white",
+    documentacion: "https://docs.github.com/es"
 },
 {
-    icono: <FaGitAlt />,
+    icono: FaGitAlt,
     tecnologia: "Git",
-    color: "",
-    documentacion: ""
+    color: "red.500",
+    documentacion: "https://git-scm.com/docs"
 },
 {
-    icono: <SiInsomnia />,
+    icono: SiInsomnia,
     tecnologia: "Insomnia REST",
-    color: "",
-    documentacion: ""
+    color: "purple.500",
+    documentacion: "https://support.insomnia.rest/"
 },
 {
-    icono: <SiVisualstudiocode />,
+    icono: SiVisualstudiocode,
     tecnologia: "Visual Studio Code",
-    color: "",
-    documentacion: ""
+    color: "blue.500",
+    documentacion: "https://code.visualstudio.com/docs"
 }]
 
 export const lenguajes = [{
-    icono: <FaPhp />,
+    icono: FaPhp,
     tecnologia: "PHP",
-    color: "purple.500",
+    color: "white",
     documentacion: "https://www.php.net/docs.php"
 },
 {
-    icono: <SiJavascript />,
+    icono: SiJavascript,
     tecnologia: "Javascript",
     color: "yellow.400",
     documentacion: "https://devdocs.io/javascript/"
-}]
+},
+{
+    icono: FaJava,
+    tecnologia: "Java",
+    color: "red.500",
+    documentacion: "https://docs.oracle.com/en/java/javase/16/docs/api/index.html"
+},
+]
 
 export const base_datos = [{
-    icono: <SiPostgresql />,
+    icono: SiPostgresql,
     tecnologia: "PostgresSQL",
-    color: "",
-    documentacion: ""
+    color: "blue.500",
+    documentacion: "https://www.postgresql.org/docs/"
 },
 {
-    icono: <SiMysql />,
+    icono: SiMysql,
     tecnologia: "MySQL",
-    color: "",
-    documentacion: ""
+    color: "blue.500",
+    documentacion: "https://dev.mysql.com/doc/"
 },
 {
-    icono: <SiMongodb />,
+    icono: SiMongodb,
     tecnologia: "MongoDB",
-    color: "",
-    documentacion: ""
+    color: "green.500",
+    documentacion: "https://docs.mongodb.com/"
 }]
